142d25a476a25b100918ae1e134dab77427c921b
rabindra12345 <rabindradahal2015@gmail.com>
2024-02-20 22:43:18 +0545

    initial commit

---
+++ src/main/java/com/test/EaseExam/EaseExamApplication.java
@@ -0,0 +1,13 @@
+package com.test.EaseExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaseExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/EaseExam/EaseExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.EaseExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaseExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
